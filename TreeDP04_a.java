08c33ec1d67b2b01df119f2269072224c98a7ec3
Deepak <blrhyd0@gmail.com>
2022-08-21 23:41:16 +0530

    Update TreeDP04_a.java

--- TreeDP04_a.java
+++ TreeDP04_a.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 //Not complete just for ref
 class TreeDP04_a {
 
-    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+  static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
     static PrintWriter out = new PrintWriter(System.out);
     /*
     use in for reading input
@@ -20,10 +20,10 @@ class TreeDP04_a {
     static int[] parent;
 
     static void dfs(int node, int l) {
-        level[node] = 1;
+        level[node] = l;
         for (int c : child[node]) {
             parent[c] = node;
-            dfs(c, 1 + 1);
+            dfs(c, l + 1);
         }
     }
 
@@ -74,7 +74,7 @@ class TreeDP04_a {
 
         int q = Integer.parseInt(in.readLine());
 
-        while (q-- > 0) {
+        while (q-->0) {
             String inp[] = in.readLine().split(" ");
             int u = Integer.parseInt(inp[0]);
             int v = Integer.parseInt(inp[1]);
@@ -86,7 +86,7 @@ class TreeDP04_a {
     public static void main(String[] args) throws IOException {
         int t = Integer.parseInt(in.readLine());
         for (int i = 1; i <= t; i++) {
-            out.println("Case " + i + ":");
+            out.println("Case "+i+":");
             solve();
         }
         out.close();
