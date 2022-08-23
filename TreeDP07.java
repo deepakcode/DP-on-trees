ea3cb050bc2b357817ae3f8faaf0c02ca966be25
Deepak <blrhyd0@gmail.com>
2022-08-23 18:24:37 +0530

    Add files via upload

---
+++ TreeDP07.java
@@ -0,0 +1,67 @@
+package com.practice.test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class TreeDP07 {
+
+    //Longest Good Segment
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter out = new PrintWriter(System.out);
+    /*
+    use in for reading input
+    use out for printing output
+    */
+
+    static int[] minStart;
+
+    static int cal(int max, int k) {
+        int min = max;
+        for (int i = 1; i <= k && min > -1; i++) {
+            min = minStart[min];
+        }
+        return max - min;
+    }
+
+    static void solve() throws IOException {
+        int ar[] = read(in.readLine());// [n, k, s]
+        int n = ar[0];
+        int k = ar[1];
+        int s = ar[2];
+        minStart = new int[n];
+        int nums[] = read(in.readLine());
+        int j = 0, sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            while (sum > s) {
+                sum -= nums[j];
+                j++;
+            }
+            minStart[i] = j - 1;
+        }
+        int max = 0;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            max = Math.max(max, cal(i, k));
+        }
+        out.println(max);
+    }
+
+    static int[] read(String s) {// nk s
+        String inp[] = s.split(" ");
+        int ar[] = new int[inp.length];
+        for (int i = 0; i < inp.length; i++) {
+            ar[i] = Integer.parseInt(inp[i]);
+        }
+        return ar;
+    }
+
+    public static void main(String[] args) throws IOException {
+        int t = Integer.parseInt(in.readLine());
+        while (t-- > 0) {
+            solve();
+        }
+        out.close();
+    }
+}
